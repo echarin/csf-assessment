ebbd3e94d4abc45916c1bcb6930c7527f3d5d22f
echarin <wanmingkang@gmail.com>
2023-05-12 09:39:14 +0800

    application properties set

--- archive_frontend/src/environments/environment.development.ts
+++ archive_frontend/src/environments/environment.development.ts
@@ -1,1 +1,6 @@
-export const environment = {};
+export const environment = {
+    production: true,
+    sbApiUrl: 'http://localhost:8080' // For local testing
+    // sbApiUrl: 'https://csf-day39-workshop-combined-production.up.railway.app' // Cross-origin
+    // sbApiUrl: '' // Same-origin
+};
