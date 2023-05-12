eaedb5ee2a472c84981ca6da0cf50e2c303bfe1a
echarin <wanmingkang@gmail.com>
2023-05-12 16:19:16 +0800

    angular environment variables change

--- archive_frontend/src/environments/environment.ts
+++ archive_frontend/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    sbApiUrl: 'http://localhost:8080' // For local testing
+    // sbApiUrl: 'http://localhost:8080' // For local testing
     // sbApiUrl: 'https://csf-day39-workshop-combined-production.up.railway.app' // Cross-origin
-    // sbApiUrl: '' // Same-origin
+    sbApiUrl: '' // Same-origin
 };
